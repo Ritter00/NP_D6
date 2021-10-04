c2d9c40ee5df1a46b7f560cca3e2987982ecccb5
OlegZ <olzim@tut.by>
2021-10-04 20:57:32 +0300

    home

--- NewsPaper/myapp/urls.py
+++ NewsPaper/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import PostList, PostDetail, PostListSearch, PostCreateView, PostUpdateView, PostDeleteView
-from .views import upgrade_me, CategoryList, CategoryDetail, subscriber, not_subscriber
+from .views import upgrade_me, CategoryList, CategoryDetail, subscriber, is_subscriber
 
 urlpatterns = [
     path('', PostList.as_view()),
@@ -11,8 +11,8 @@ urlpatterns = [
     path('delete/<int:pk>', PostDeleteView.as_view(), name='post_delete'),
     path('create/upgrade/', upgrade_me, name='upgrade'),
     path('category/', CategoryList.as_view(), name='categories' ),
-    path('category/<int:pk>', CategoryDetail.as_view(),name='category_detail'),
-    path('category/<int:pk>/', subscriber, name='subscriber'),
-    path('category/<int:pk>/', not_subscriber, name='not_subscriber'),
+    path('category/<int:pk>', CategoryDetail.as_view(), name='category_detail'),
+    path('category/<int:pk>', subscriber, name='subscriber'),
+    path('category/<int:pk>', is_subscriber, name='is_subscriber'),
 
     ]
--- NewsPaper/myapp/views.py
+++ NewsPaper/myapp/views.py
@@ -12,24 +12,23 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.http import HttpResponse
 
 
-
-
 class PostList(ListView):
     model = Post
     template_name = 'posts.html'
     context_object_name = 'posts'
-    queryset = Post.objects.order_by('-id') #  сортируем, еще можно через ordering = ['-id']
-    paginate_by = 7 # поставим постраничный вывод в n-элемент
+    queryset = Post.objects.order_by('-id')  # сортируем, еще можно через ordering = ['-id']
+    paginate_by = 7  # поставим постраничный вывод в n-элемент
     form_class = PostForm
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs) # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса
-        context['filter']= PostFilter(self.request.GET, queryset=self.get_queryset())
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(
+            **kwargs)  # забираем отфильтрованные объекты переопределяя метод get_context_data у наследуемого класса
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
         context['form'] = PostForm()
         context['is_not_authors'] = not self.request.user.groups.filter(name='authors').exists()
         return context
 
-    def post(self, request,*args, **kwargs):
+    def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
 
         if form.is_valid():
@@ -37,24 +36,24 @@ class PostList(ListView):
 
         return super().get(request, *args, **kwargs)
 
+
 class PostDetail(DetailView):
-    #model = Post
-    #template_name = 'post.html'
-    #context_object_name = 'post'
+    # model = Post
+    # template_name = 'post.html'
+    # context_object_name = 'post'
     template_name = 'post_detail.html'
     queryset = Post.objects.all()
 
 
-class PostListSearch(ListView,):
+class PostListSearch(ListView, ):
     model = Post
     template_name = 'posts_search.html'
     context_object_name = 'posts'
-    queryset = Post.objects.order_by('-id') #  сортируем, еще можно через ordering = ['-id']
-    paginate_by = 10 # поставим постраничный вывод в n-элемент
-
+    queryset = Post.objects.order_by('-id')  # сортируем, еще можно через ordering = ['-id']
+    paginate_by = 10  # поставим постраничный вывод в n-элемент
 
     def get_filter(self):
-        return PostFilter(self.request.GET, queryset= super().get_queryset())
+        return PostFilter(self.request.GET, queryset=super().get_queryset())
 
     def get_queryset(self):
         return self.get_filter().qs
@@ -62,13 +61,13 @@ class PostListSearch(ListView,):
     def get_context_data(self, *args, **kwargs):
         return {
             **super().get_context_data(*args, **kwargs),
-            'filter' : self.get_filter(),
+            'filter': self.get_filter(),
         }
 
 
-class PostCreateView(PermissionRequiredMixin,CreateView):
+class PostCreateView(PermissionRequiredMixin, CreateView):
     template_name = 'post_create.html'
-    permission_required = ('myapp.add_post', )
+    permission_required = ('myapp.add_post',)
     form_class = PostForm
 
     def get_context_data(self, **kwargs):
@@ -77,22 +76,19 @@ class PostCreateView(PermissionRequiredMixin,CreateView):
         return context
 
 
-class PostUpdateView(LoginRequiredMixin,PermissionRequiredMixin, UpdateView):
+class PostUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     template_name = 'post_create.html'
     permission_required = ('myapp.change_post')
     form_class = PostForm
 
-
     def get_object(self, **kwargs):
-        id= self.kwargs.get('pk')
-        return  Post.objects.get(pk= id)
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['is_not_authors'] = not self.request.user.groups.filter(name='authors').exists()
         return context
-
-
 
 
 class PostDeleteView(DeleteView):
@@ -121,30 +117,28 @@ class CategoryDetail(LoginRequiredMixin, DetailView):
     template_name = 'category_detail.html'
     queryset = Category.objects.all()
 
-
-
     def get_context_data(self, **kwargs):
-        context= super().get_context_data(**kwargs)
-        context['if_not_subscriber'] = Category.objects.filter(id=kwargs['object'].id ,subscribers=self.request.user)
-
+        context = super().get_context_data(**kwargs)
+        # context['if_not_subscriber'] = Category.objects.filter(id=kwargs['object'].id, subscribers=self.request.user)
+        us = Category.objects.get(id=kwargs['object'].id)
+        context['if_not_subscriber'] = us.subscribers.filter(id=self.request.user.id).last()
+        context['re'] = us.subscribers.all()
         return context
-
 
 
 def subscriber(request, pk):
     user = request.user.id
     sub_user = User.objects.get(id=user)
-    #pk= request.META.get('HTTP_REFERER')[-1]
+    # pk= request.META.get('HTTP_REFERER')[-1]
 
     cat = Category.objects.get(id=pk)
     cat.subscribers.add(sub_user)
     return redirect(request.META.get('HTTP_REFERER'))
 
 
-def not_subscriber(request, pk):
+def is_subscriber(request, pk):
     user = request.user.id
     sub_user = User.objects.get(id=user)
-    #pk= request.META.get('HTTP_REFERER')[-1]
     cat = Category.objects.get(id=pk)
     cat.subscribers.remove(sub_user)
     return redirect(request.META.get('HTTP_REFERER'))
